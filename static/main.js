1964b84b4538f37841c811810ac72705496222bd
ITAD <pythondongwonrhee@gmail.com>
2019-03-06 16:12:14 -0500

    main with css js

--- static/main.js
+++ static/main.js
@@ -1,5 +1,14 @@
+$(document).ready(function () {
 $('ul.nav li.dropdown').hover(function() {
   $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
 }, function() {
   $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
 });
+});
+
+//
+//$(document).ready(function () {
+//    $("nav").find("li").on("click", "a", function () {
+//        $('.navbar-collapse.in').collapse('hide');
+//    });
+//});
