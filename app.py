7ba3c5cd511df3135da07e937184df0f01ee83c2
ITAD <pythondongwonrhee@gmail.com>
2019-03-12 10:11:29 -0400

    change logins include

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import *
 
 app = Flask(__name__)
 
+username = 'fffffffffff'
+
 
 @app.route('/')
 def index():
@@ -10,15 +12,17 @@ def index():
 
 @app.route('/hello')
 def hello():
-    return render_template('hello.html')
+    return render_template('hello.html', username=username)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     loginMsg = ''
     if request.method == 'POST':
+        global username
+        username= request.form['username']
         if request.form['username'] == 'guest' and request.form['password'] == 'guest':
-            return render_template('hello.html')
+            return redirect(url_for('hello'))
         else:
             loginMsg = 'Invalid username or password, Please try again.'
     return render_template('login.html', loginMsg=loginMsg)
@@ -26,12 +30,13 @@ def login():
 
 @app.route('/slideshow')
 def slideshow():
-    return render_template('slideshow.html')
+    return render_template('slideshow.html', username=username)
 
 
 @app.route('/main')
 def main():
-    return render_template('main.html')
+    return render_template('main.html', username=username)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ static/login.js
@@ -0,0 +1,3 @@
+
+alert("Hello! I am an alert box!!");
+//$('input[name=username]').focus();
